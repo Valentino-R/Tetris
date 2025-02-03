eefd4fb2ba3a0e0992de939f952458b2013ea957
valentino rodriguez <valentino.orif@gmail.com>
2025-02-03 17:45:20 +0100

    modif code

--- Tetris.cpp
+++ Tetris.cpp
@@ -69,41 +69,13 @@ int main ()
     SetConsoleActiveScreenBuffer(hConsole);
     DWORD dwBytesWritten = 0;
 
-    //creatre assets
-    tetromino[0].append(L"..X.");
-    tetromino[0].append(L"..X.");
-    tetromino[0].append(L"..X.");
-    tetromino[0].append(L"..X.");
-
-    tetromino[1].append(L"..X.");
-    tetromino[1].append(L".XX.");
-    tetromino[1].append(L".X..");
-    tetromino[1].append(L"....");
-
-    tetromino[2].append(L".X..");
-    tetromino[2].append(L".XX.");
-    tetromino[2].append(L"..X.");
-    tetromino[2].append(L"....");
-
-    tetromino[3].append(L"....");
-    tetromino[3].append(L".XX.");
-    tetromino[3].append(L".XX.");
-    tetromino[3].append(L"....");
-
-    tetromino[4].append(L"..X.");
-    tetromino[4].append(L".XX.");
-    tetromino[4].append(L"..X.");
-    tetromino[4].append(L"....");
-
-    tetromino[5].append(L"....");
-    tetromino[5].append(L".XX.");
-    tetromino[5].append(L"..X.");
-    tetromino[5].append(L"..X.");
-
-    tetromino[6].append(L"....");
-    tetromino[6].append(L".XX.");
-    tetromino[6].append(L".X..");
-    tetromino[6].append(L".X..");
+    tetromino[0].append(L"..X...X...X...X."); // Tetronimos 4x4
+	tetromino[1].append(L"..X..XX...X.....");
+	tetromino[2].append(L".....XX..XX.....");
+	tetromino[3].append(L"..X..XX..X......");
+	tetromino[4].append(L".X...XX...X.....");
+	tetromino[5].append(L".X...X...XX.....");
+	tetromino[6].append(L"..X...X..XX.....");
 
     pField = new unsigned char[nFieldWidth*nFieldHeight];// Create playfield buffer
     for (int x = 0; x < nFieldWidth; x++)// Board Boundary
